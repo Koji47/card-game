7e5afd1112902f686b6b19ef056944fb9a77ea93
Chu <Chu@github.com>
2023-09-27 16:00:02 +0100

    Multiplayer works but timer is still not fully functioning

--- src/main/java/snap/Main.java
+++ src/main/java/snap/Main.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         CardGame cardGame = new CardGame();
-//        SnapGame snap = new SnapGame();
-        Snap snapSinglePlayer = new Snap();
-        snapSinglePlayer.playSnap();
-//        snap.playSnapGame();
+        SnapGame snap = new SnapGame();
+//        Snap snapSinglePlayer = new Snap();
+//        snapSinglePlayer.playSnap();
+        snap.playSnapGame();
     }
 }
--- src/main/java/snap/SnapGame.java
+++ src/main/java/snap/SnapGame.java
@@ -1,8 +1,6 @@
 package snap;
 
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.*;
 
 public class SnapGame extends CardGame {
@@ -10,55 +8,55 @@ public class SnapGame extends CardGame {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
-//    private Timer snapTimer;
-//    private ScheduledExecutorService snapExecutor;
+    private ScheduledExecutorService snapExecutor;
 
     public SnapGame() {
         this.scanner = new Scanner(System.in);
         this.player1 = new Player("Player 1");
         this.player2 = new Player("Player 2");
         this.currentPlayer = player1;
+        this.snapExecutor = Executors.newSingleThreadScheduledExecutor();
+
     }
 
     public void playSnapGame() {
-//        shuffleDeck();
-        sortDeckInNumberOrder();
+        shuffleDeck();
+//        sortDeckInNumberOrder();
         System.out.println("Player 1 press Enter to take your turn");
 
         boolean snap = false;
-        boolean snapOpportunity = false;
-        long snapOpportunityStartTime = 0;
-
+        ScheduledFuture<?> scheduledTask = null;
         while (!snap) {
             String input = scanner.nextLine();
+
             if (input.equalsIgnoreCase("V>P")) {
                 System.out.println("༼つಠ益ಠ༽つ ─=≡ΣO)) hadouken");
-            } else {
-                if (canSnap() && !snapOpportunity) {
-                    snapOpportunity = true;
-                    snapOpportunityStartTime = System.currentTimeMillis();
-                    System.out.println("Snap opportunity ~(‾▿‾)~ type 'snap' within 2 seconds to win.");
-                } else if (snapOpportunity) {
-                    long timeDifference = System.currentTimeMillis() - snapOpportunityStartTime;
+            } else if (canSnap()) {
+                scheduledTask = snapExecutor.schedule(()->{ // scheduled task after 2 seconds
+                    System.out.println(currentPlayer.getName() + " missed the snap");
+                }, 2, TimeUnit.SECONDS);
+                System.out.println("Snap opportunity ~(‾▿‾)~ type 'snap' within 2 seconds to win.");
 
-                    if (input.equalsIgnoreCase("snap") && timeDifference < 2000) {
-                        System.out.println("Snap! (∩╹□╹∩) " + currentPlayer.getName() + " wins \\m/ (>.<) \\m/ ");
-                        break;
-                    } else if (timeDifference > 2000) {
-                        System.out.println(currentPlayer.getName() + " missed the snap");
-                    }
-
-                } else {
-                    Card currentCard = dealCard();
-                    System.out.println(currentPlayer.getName() + " drew: " + currentCard);
-                    currentPlayer = getOpponent();
+            } else if (input.equalsIgnoreCase("snap")) {
+                if (scheduledTask != null) {
+                    scheduledTask.cancel(true);
                 }
+                snapExecutor.shutdown(); // cancels the scheduled task
+                System.out.println("0Snap! (∩╹□╹∩) " + currentPlayer.getName() + " wins \\m/ (>.<) \\m/ ");
+                break;
+            } else if (canSnap()){
+                snapExecutor.shutdown(); // cancels the scheduled task
+                System.out.println(currentPlayer.getName() + " missed the snap");
+                break;
+            } else {
+                Card currentCard = dealCard();
+                System.out.println(currentPlayer.getName() + " drew: " + currentCard);
+                currentPlayer = getOpponent();
             }
         }
         scanner.close();
+        snapExecutor.shutdown();
     }
-
-
 
     private Player getOpponent() {
         return (currentPlayer == player1) ? player2 : player1;
@@ -66,11 +64,8 @@ public class SnapGame extends CardGame {
 
     private boolean canSnap() {
         Card previousCard = getPreviousCard();
-        boolean canSnap = previousCard != null && previousCard.getSymbol().equals(getCurrentCard().getSymbol());
-        return canSnap;
+        return previousCard != null && previousCard.getSymbol().equals(getCurrentCard().getSymbol());
     }
 
 
 }
-
-
